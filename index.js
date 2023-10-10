942e018d2c38abc63827e2c102547d28c8923680
LiisiM <liisi.muur@gmail.comm>
2023-10-10 18:42:15 +0300

    User mapping finished

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const url = "https://jsonplaceholder.typicode.com/users"
+const url = "https://jsonplaceholder.typicode.com/users";
+const users = document.querySelector("#users");
 
 const getUsers = async () => {
     const res = await fetch(url);
@@ -6,6 +7,25 @@ const getUsers = async () => {
         throw new Error(`HTTP error! status: ${res.status}`);
     }
     const data = await res.json();
-    console.log(data);
+    return data;
 }
-getUsers();
+
+const displayUsers = async () => {
+    const payload = await getUsers();
+    let dataUsers = payload.map((user) => {
+        const { name, username, email } = user;
+        const fullName = name.split(' ');
+        const firstName = fullName[0];
+        const lastName = fullName.slice(1).join(' ');
+        return `
+        <tr>
+        <td>${username}</td>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${email}</td>
+        </tr>
+        `
+    }).join('');
+    users.innerHTML = dataUsers;
+}
+displayUsers();
