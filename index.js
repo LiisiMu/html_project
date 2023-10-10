883fc92c6acf0005306ec73b55740399ec3f1adb
LiisiM <liisi.muur@gmail.comm>
2023-10-10 19:55:10 +0300

    User filtering works

--- index.js
+++ index.js
@@ -10,22 +10,42 @@ const getUsers = async () => {
     return data;
 }
 
-const displayUsers = async () => {
-    const payload = await getUsers();
-    let dataUsers = payload.map((user) => {
-        const { name, username, email } = user;
-        const fullName = name.split(' ');
-        const firstName = fullName[0];
-        const lastName = fullName.slice(1).join(' ');
-        return `
-        <tr>
-        <td>${username}</td>
-        <td>${firstName}</td>
-        <td>${lastName}</td>
-        <td>${email}</td>
-        </tr>
-        `
-    }).join('');
-    users.innerHTML = dataUsers;
+const displayUsers = async (usersToDisplay) => {
+    if (usersToDisplay.length === 0) {
+        users.innerHTML = "Ühtegi kasutajat ei leitud. Proovi uuesti.";
+    } else {
+        let dataUsers = usersToDisplay.map((user) => {
+            const { name, username, email } = user;
+            const fullName = name.split(' ');
+            const firstName = fullName[0];
+            const lastName = fullName.slice(1).join(' ');
+            return `
+            <tr>
+            <td>${username}</td>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${email}</td>
+            </tr>
+            `
+        }).join('');
+        users.innerHTML = dataUsers;
+    }
+
 }
-displayUsers();
+const searchUsers = async () => {
+    const username = document.querySelector('#usernameInput').value.toLowerCase();
+    const firstName = document.querySelector('#firstNameInput').value.toLowerCase();
+    const lastName = document.querySelector('#lastNameInput').value.toLowerCase();
+
+    const usersData = await getUsers();
+    const filteredUsers = usersData.filter(user => {
+        const fullName = user.name.split(' ');
+        const userFirstName = fullName[0].toLowerCase();
+        const userLastName = fullName.slice(1).join(' ').toLowerCase();
+        return (username ? user.username.toLowerCase().includes(username) : true) &&
+            (firstName ? userFirstName.includes(firstName) : true) &&
+            (lastName ? userLastName.includes(lastName) : true);
+    });
+    displayUsers(filteredUsers);
+}
+searchUsers();
