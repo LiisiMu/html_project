c31b113e0920bd443705b4236e2a17ed5deedf20
LiisiM <liisi.muur@gmail.comm>
2023-10-12 18:12:56 +0300

    added styling, modal and delete button funct

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ lastNameInput.addEventListener('keydown', function (e) {
 });
 
 const getUsers = async () => {
-    const res = await fetch(url);
-    if (!res.ok) {
-        throw new Error(`HTTP error! status: ${res.status}`);
-    }
-    const data = await res.json();
+            const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        const data = await res.json();
     return data;
 }
 
@@ -38,31 +38,39 @@ const displayUsers = async (usersToDisplay) => {
         table.style.display = 'table';
     } else {
         let dataUsers = usersToDisplay.map((user) => {
-            const { name, username, email } = user;
+            const { name, username, email, id } = user;
             const fullName = name.split(' ');
             const firstName = fullName[0];
             const lastName = fullName.slice(1).join(' ');
             return `
-            <tr>
+            <tr id=row-${id}>
             <td>${username}</td>
             <td>${firstName}</td>
             <td>${lastName}</td>
             <td>${email}</td>
+            <td><i id=delete-${id} class="userbutton fa-sharp fa-solid fa-user-minus"></i>
+            </td>
             </tr>
             `
         }).join('');
         table.style.display = 'table';
         users.innerHTML = dataUsers;
     }
+    usersToDisplay.forEach((user) => {
+        const deleteButton = document.querySelector(`#delete-${user.id}`);
+
+        deleteButton.addEventListener('click', () => {
+            document.querySelector(`#row-${user.id}`).remove();
+        });
+    });
 }
 
 const searchUsers = async () => {
     const username = document.querySelector('#usernameInput').value.toLowerCase();
     const firstName = document.querySelector('#firstNameInput').value.toLowerCase();
     const lastName = document.querySelector('#lastNameInput').value.toLowerCase();
-
-
     const usersData = await getUsers();
+
     const filteredUsers = usersData.filter(user => {
         const fullName = user.name.split(' ');
         const userFirstName = fullName[0].toLowerCase();
