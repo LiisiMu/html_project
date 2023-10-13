fe8a4a39ebc97102dd8557a15fe714efb1914eae
LiisiM <liisi.muur@gmail.comm>
2023-10-13 16:19:39 +0300

    commented out add user and delete user

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 const url = "https://jsonplaceholder.typicode.com/users";
 const users = document.querySelector("#users");
 const table = document.querySelector('.table');
+let usersArray = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : [];
 
 const usernameInput = document.querySelector('#usernameInput');
 const firstNameInput = document.querySelector('#firstNameInput');
 const lastNameInput = document.querySelector('#lastNameInput');
+
 const clearSearchLink = document.querySelector('#clearSearch');
 
-usernameInput.addEventListener('keydown', function (e) {
-    if (e.key === 'Enter') {
-        searchUsers();
-    }
-});
-firstNameInput.addEventListener('keydown', function (e) {
-    if (e.key === 'Enter') {
-        searchUsers();
-    }
-});
-lastNameInput.addEventListener('keydown', function (e) {
-    if (e.key === 'Enter') {
-        searchUsers();
-    }
-});
+const addUserButton = document.querySelector('#addUser');
+const addUserModal = new bootstrap.Modal(document.getElementById('addUserModal'));
+const saveUserButton = document.querySelector('#saveButton');
 
 const getUsers = async () => {
-            const res = await fetch(url);
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    const data = await res.json();
+    const modifiedData = data.map(user => {
+        return {
+            id: user.id,
+            username: user.username,
+            name: user.name,
+            email: user.email
         }
-        const data = await res.json();
-        localStorage.setItem("users", JSON.stringify(data))
-    return data;
+    });
+    localStorage.setItem('users', JSON.stringify(modifiedData))
+    return modifiedData;
 }
 
 const displayUsers = async (usersToDisplay) => {
@@ -57,19 +55,48 @@ const displayUsers = async (usersToDisplay) => {
         table.style.display = 'table';
         users.innerHTML = usersData;
     }
-    usersToDisplay.forEach((user) => {
-        const deleteButton = document.querySelector(`#delete-${user.id}`);
+    // usersToDisplay.forEach((user) => {
+    //     const deleteButton = document.querySelector(`#delete-${user.id}`);
+    
+    //     deleteButton.addEventListener('click', () => {
+    //         const updatedUsers = usersToDisplay.filter(user => user.id !== user.id);
+    //         localStorage.setItem('users', JSON.stringify(updatedUsers));
+    //         document.querySelector(`#row-${user.id}`).remove();
+    //     });
+    // });
+}
 
-        deleteButton.addEventListener('click', () => {
-            document.querySelector(`#row-${user.id}`).remove();
-        });
-    });
+usernameInput.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        searchUsers();
+    }
+});
+firstNameInput.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        searchUsers();
+    }
+});
+lastNameInput.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        searchUsers();
+    }
+});
+
+const showNotification = (message, className) => {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const table = document.querySelector('#usersTable');
+    container.insertBefore(div, table);
+
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
 }
 
 const searchUsers = async () => {
-    const username = document.querySelector('#usernameInput').value.toLowerCase();
-    const firstName = document.querySelector('#firstNameInput').value.toLowerCase();
-    const lastName = document.querySelector('#lastNameInput').value.toLowerCase();
+    const username = document.querySelector('#usernameInput').value.toLowerCase().trim();
+    const firstName = document.querySelector('#firstNameInput').value.toLowerCase().trim();
+    const lastName = document.querySelector('#lastNameInput').value.toLowerCase().trim();
     const usersData = await getUsers();
 
     const filteredUsers = usersData.filter(user => {
@@ -91,3 +118,40 @@ clearSearchLink.addEventListener('click', () => {
 
     table.style.display = 'none';
 });
+
+// addUserButton.addEventListener('click', () => {
+//     addUserModal.show();
+// });
+
+
+// saveUserButton.addEventListener('click', (e) => {
+//     e.preventDefault();
+
+//     const newUsernameInput = document.querySelector('#newUsername');
+//     const newFirstNameInput = document.querySelector('#newFirstName');
+//     const newLastNameInput = document.querySelector('#newLastName');
+//     const newEmailInput = document.querySelector('#newEmail');
+
+//     const newUser = {
+//         id: Date.now(),
+//         username: newUsernameInput.value,
+//         name: `${newFirstNameInput.value} ${newLastNameInput.value}`,
+//         email: newEmailInput.value
+//     };
+
+//     const currentUsers = JSON.parse(localStorage.getItem('users')) || [];
+//     currentUsers.push(newUser);
+
+//     localStorage.setItem('users', JSON.stringify(currentUsers));
+
+//     displayUsers(currentUsers);
+
+//     addUserModal.hide();
+
+//     document.querySelector('#newUsername').value = '';
+//     document.querySelector('#newFirstName').value = '';
+//     document.querySelector('#newLastName').value = '';
+//     document.querySelector('#newEmail').value = '';
+
+//     showNotification('Kasutaja lisatud', 'success')
+// });
